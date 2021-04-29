ee652bc72939cc46ff7c2fb811ea74ff5bbb4a6a
flaviasoares <flaviasoares@ic.ufa.br>
2021-04-28 22:25:02 -0300

    Estruturas condicionais

---
+++ Conditional_Statements/Conditional_Statements.ml
@@ -0,0 +1,6 @@
+(*STRUCTURE: if boolean then something else something*)
+
+if (5 > 3) then print_int 5 else print_int 3;; (*se 5 for maior que três, então imprima 5, senão imprima 3*)
+print_string "\n";;
+if (5 > 3) then print_string "this is true" else print_string "this is false";;
+print_string "\n";; (*se 5 maior que 3, então imprima "this is true", senão imprima "this is false"*)
