a83cc2f42e8bf86220b080d508ede15c8e5c24c6
flaviasoares <flaviasoares@ic.ufa.br>
2021-04-28 21:20:26 -0300

    Adicionada formas de exibir algum tipo de dado na tela

--- Primitives_and_Expressions/Primitives_and_Expressions.ml
+++ Primitives_and_Expressions/Primitives_and_Expressions.ml
@@ -1,1 +1,5 @@
-print_string "Exemple\n";;
+print_string "Objective Categorical Abstract Machine Language\n";; (*imprime Exemple e pula uma linha*)
+print_int 5;; (*imprime 5 *)
+print_string "\n";; (*pula uma linha*)
+print_int (5 * 5);; (*imprime o resultado de 5 x 5, ou seja, 25*)
+print_string "\n";; (*pula uma linha*)
